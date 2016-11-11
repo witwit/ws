9e338155d1de17518a1cfcb61662ca5b2e4728ec
Philipp Zins <philipp.zins@mercateo.com>
2016-11-11 13:03:24 +0100

    quick fix for our config

--- src/actions/watch.ts
+++ src/actions/watch.ts
@@ -44,6 +44,8 @@ export default async function watch() {
         // this is a quich fix to get relative path for assets in localized spa's working
         await Promise.all(project.ws.i18n.locales.map(locale =>
           copy(project.ws.distDir, join(project.ws.distDir, locale), '*.{png,jpg,gif,svg,eot,woff,woff2,ttf}')));
+        await Promise.all(project.ws.i18n.locales.map(locale =>
+            copy(join(project.ws.distDir, 'config'), join(project.ws.distDir, locale, 'config'), '*.js')));
       }
       break;
     case TYPE.BROWSER:
