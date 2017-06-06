78f2ea26f96a45f4f0a00ea1baa96e5814d5c39b
Philipp Zins <philipp.zins@mercateo.com>
2017-06-06 10:27:47 +0200

    quick fix for update notifier

--- src/lib/update-notifier.ts
+++ src/lib/update-notifier.ts
@@ -15,34 +15,58 @@ const getRandom = (arr: Array<any>): any =>
   arr[Math.floor(Math.random() * arr.length)];
 
 export function updateNotifier(version: string) {
-  return new Promise(resolve =>
-    exec(`npm show @mercateo/ws dist-tags --json`, (error, stdout, stderr) => {
-      if (error) {
-        warn(`Couldn't check, if a new version of @mercateo/ws is available.`);
-        warn(error);
-        resolve();
-      } else {
-        if (stderr.trim()) {
-          warn(stderr);
+  return new Promise(resolve => {
+    let state: 'pending' | 'timeout' | 'finished' = 'pending';
+
+    const child = exec(
+      `npm show @mercateo/ws dist-tags --json`,
+      (error, stdout, stderr) => {
+        if (state === 'timeout') {
+          warn(
+            'Timeout while trying to check, if a new version of @mercateo/ws is available.'
+          );
+          return resolve();
+        } else {
+          state = 'finished';
         }
 
-        const distTags = JSON.parse(stdout) as DistTags;
-        const isPrerelease = version.includes('-');
-        const remoteVersion = isPrerelease ? distTags.next : distTags.latest;
-        if (gt(remoteVersion, version)) {
-          info(`
+        if (error) {
+          warn(
+            `Couldn't check, if a new version of @mercateo/ws is available.`
+          );
+          warn(error);
+          resolve();
+        } else {
+          if (stderr.trim()) {
+            warn(stderr);
+          }
+
+          const distTags = JSON.parse(stdout) as DistTags;
+          const isPrerelease = version.includes('-');
+          const remoteVersion = isPrerelease ? distTags.next : distTags.latest;
+          if (gt(remoteVersion, version)) {
+            info(`
   ${magenta(
     getRandom(LOVE_EMOJI)
   )} Update available. The newest version of @mercateo/ws is ${magenta(
-            remoteVersion
-          )}.
+              remoteVersion
+            )}.
   See ${cyan(CHANGELOG_URL)} for details.
 `);
-        } else {
-          debug('Your version of @mercateo/ws is up to date.');
+          } else {
+            debug('Your version of @mercateo/ws is up to date.');
+          }
+          resolve();
         }
-        resolve();
       }
-    })
-  );
+    );
+
+    // use timeout, so ws can be used without (or slow) internet connection
+    setTimeout(() => {
+      if (state === 'pending') {
+        state = 'timeout';
+        child.kill();
+      }
+    }, 3000);
+  });
 }
