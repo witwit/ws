623e5e88697a58982323dec1ab6d7b29b0ca9eb5
Benjamin Otto <benjamin.otto@mercateo.com>
2017-06-07 21:35:46 +0200

    enhanced electron workflow

--- src/lib/webpack/electron.ts
+++ src/lib/webpack/electron.ts
@@ -14,31 +14,49 @@ import {
 } from './options';
 import { project } from '../../project';
 
-export const getElectronDevOptions = (): WebpackSingleConfig => ({
-  entry: project.ws.srcEntry,
-  output: {
-    ...outputDev,
-    filename: '[name].js'
-  },
-  module: getModuleConfig('build'),
-  plugins: [indexHtmlPlugin, extractCssPlugin, loaderOptionsPlugin],
-  target: 'electron',
-  externals: project.ws.externals ? [project.ws.externals] : [],
-  performance: {
-    hints: false
-  },
-  resolveLoader,
-  resolve,
-  devtool
-});
+export const getElectronDevOptions = (): Array<WebpackSingleConfig> => {
+  const defaultConfig = {
+    output: {
+      ...outputDev,
+      filename: '[name].js'
+    },
+    node: {
+      __dirname: false,
+      __filename: false
+    },
+    module: getModuleConfig('build'),
+    plugins: [indexHtmlPlugin, extractCssPlugin, loaderOptionsPlugin],
+    externals: project.ws.externals ? [project.ws.externals] : [],
+    resolveLoader,
+    resolve,
+    devtool
+  };
+
+  return ([
+    {
+      ...defaultConfig,
+      target: 'electron-main',
+      entry: {
+        electron: project.ws.srcElectronEntry
+      }
+    },
+    {
+      ...defaultConfig,
+      target: 'electron-renderer',
+      entry: {
+        index: project.ws.srcEntry
+      }
+    }
+  ] as any) as Array<WebpackSingleConfig>;
+};
 
 export const getElectronReleaseOptions = () => {
   const options = getElectronDevOptions();
 
-  return {
-    ...options,
-    plugins: [...options.plugins!, defineProductionPlugin]
-  };
+  return options.map(config => ({
+    ...config,
+    plugins: [...config.plugins!, defineProductionPlugin]
+  }));
 };
 
 export const electronUnitOptions: WebpackSingleConfig = {
@@ -46,7 +64,7 @@ export const electronUnitOptions: WebpackSingleConfig = {
   output: outputTest,
   module: getModuleConfig('unit'),
   plugins: [extractCssPlugin, loaderOptionsPlugin],
-  target: 'electron',
+  target: 'electron-renderer',
   externals: enzymeExternals.concat(
     project.ws.externals ? [project.ws.externals] : []
   ),
